dca913ddaee0a0b21417aef732d9999a0fa97ba4
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-01 16:56:11 -0500

    made chats index page the root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'chats/new'
   get 'chats/show'
   get 'chats/index'
+
+  root 'chats#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
